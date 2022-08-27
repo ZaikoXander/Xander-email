e696df4056a762d5838ea023dd114b081c6c9c22
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-08-27 17:28:52 -0300

    Separate the library from the tests

--- examples/hello-world/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { sendEmail } from "../controllers/index.js"
-
-const routes = Router()
-
-routes.get("/", sendEmail)
-
-export default (app) => app.use(routes)
---
+++ index.js
@@ -0,0 +1,3 @@
+import xanderEmail from "./lib/xander-email.js"
+
+export default xanderEmail
